8318ed2a9ed4be7c8fbbdd4546ef4e3cc9c021dc
mateus <mateussantos1528@hotmail.com>
2026-04-05 11:04:55 -0300

    Ingress yaml and setup

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -13,7 +13,10 @@ sudo chown vagrant:vagrant /home/vagrant/.kube/config
 echo 'export KUBECONFIG=/home/vagrant/.kube/config' >> /home/vagrant/.bashrc
 echo 'export PATH=$PATH:/sbin' >> /home/vagrant/.bashrc
 
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
 # waiting for k3s to be ready
+sleep 10
 kubectl wait --for=condition=Ready node --all --timeout=120s
 
 # deploy app1 pod and service
@@ -24,3 +27,12 @@ kubectl apply -f /vagrant/app2.yaml
 
 # deploy app3 pod and service
 kubectl apply -f /vagrant/app3.yaml
+
+# apply ingress
+kubectl apply -f /vagrant/ingress.yaml
+
+until kubectl get ingress ingress-cool | grep -q "192.168.56.110"; do
+  echo "waiting ingress Ip..."
+  sleep 50
+done
+echo "ingress ready"
